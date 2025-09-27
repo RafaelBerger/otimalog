9dc6cf7863e51bbb8abc42e165fd9df9beeceaaf
Rafael Berger <rafaelberger.dev@gmail.com>
2025-09-27 16:36:49 -0300

    header layout done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/header.css";
+import logo from "./assets/logo.svg";
+import headerImg from "./assets/header-img.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="app-header">
+        <div className="header-info">
+          <div className="header-presentation">
+            <img src={logo} alt="LOGO" />
+            <h1>
+              Conectando distâncias,
+              <br /> Acelerando negócios
+            </h1>
+            <p>
+              Mais que transporte, a Otima.log atua como parceira estratégica de
+              empresas que valorizam agilidade, rastreabilidade e controle sobre
+              suas operações logísticas. Especializada em operação de fretes
+              fracionados, a empresa conta com uma estrutura moderna e um time
+              altamente qualificado, oferecendo soluções completas para quem
+              quer crescer com eficiência.
+            </p>
+          </div>
+          <div className="header-contact-btn">
+            <button>Nos Contate</button>
+          </div>
+        </div>
+        <div className="header-image">
+          <img src={headerImg} alt="Truck Image" />
+        </div>
+      </header>
+
+      <main>
+        <h2 className="main-title">Sobre nós</h2>
+
+        <section className="fractional-load">
+          <div>
+            <h2>Carga Fracionada com a OTIMA.LOG</h2>
+            <p>
+              Na carga fracionada, diferentes mercadorias de várias empresas
+              compartilham o mesmo veículo. Isso significa mais economia,
+              eficiência e rapidez, sem a necessidade de lotar um caminhão
+              inteiro para realizar a entrega. É a solução ideal para empresas
+              que precisam enviar quantidades menores de produtos, mas exigem a
+              mesma segurança, rastreabilidade e pontualidade de uma carga
+              completa.
+            </p>
+            <ul>
+              <li>Embarques diários</li>
+              <li>Rapidez nas entregas</li>
+              <li>Roteirização estratégica → menor custo e maior agilidade</li>
+              <li>Monitoramento em tempo real via torre de controle</li>
+              <li>Segurança e gestão de risco ativa</li>
+              <li>
+                Armazém próprio estruturado (1.000 posições-palete, 7 docas)
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <div>ILUSTRACAO</div>
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
