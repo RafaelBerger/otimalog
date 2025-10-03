61d3a0e73231f6b868d9bc0a7f75f31310b2cbc6
Rafael Berger <rafaelberger.dev@gmail.com>
2025-10-03 16:07:33 -0300

    about responsiveness is done, header ajusted, and background done

--- src/components/ContentShowcase.tsx
+++ src/components/ContentShowcase.tsx
@@ -9,7 +9,7 @@ interface ContentShowcaseProps {
 
 function ContentShowcase({ children, src, isReversed }: ContentShowcaseProps) {
   return (
-    <section className="section-container">
+    <section className={`section-container`}>
       {isReversed ? (
         <>
           <div className="image-content-reversed">
@@ -34,3 +34,40 @@ function ContentShowcase({ children, src, isReversed }: ContentShowcaseProps) {
 }
 
 export default ContentShowcase;
+
+// import "../styles/about-section-structure.css";
+
+// interface ContentShowcaseProps {
+//   children: React.ReactNode;
+//   src: string;
+//   isReversed: boolean;
+//   isLighter: boolean;
+// }
+
+// function ContentShowcase({ children, src, isReversed }: ContentShowcaseProps) {
+//   return (
+//     <section className={`section-container`}>
+//       {isReversed ? (
+//         <>
+//           <div className="image-content-reversed">
+//             <div>
+//               <img src={src} alt="Illustration" />
+//             </div>
+//           </div>
+//           <div className="text-content">{children}</div>
+//         </>
+//       ) : (
+//         <>
+//           <div className="text-content">{children}</div>
+//           <div className="image-content">
+//             <div>
+//               <img src={src} alt="Illustration" />
+//             </div>
+//           </div>
+//         </>
+//       )}
+//     </section>
+//   );
+// }
+
+// export default ContentShowcase;
